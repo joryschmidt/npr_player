70f05c8210ae8aeba9ce444815117d93703dc149
Jory Schmidt <joryschmidt1988@gmail.com>
2015-11-15 03:18:10 +0000

    add custom directive and template

--- js/main.js
+++ js/main.js
@@ -30,3 +30,19 @@ app.controller('PlayerController', function($scope, $http){
 app.controller('RelatedController', ['$scope', function($scope){
     
 }]);
+
+app.directive('nprLink', function() {
+  return {
+    restrict: 'EA',
+    require: ['^ngModel'],
+    replace: true,
+    scope: {
+      ngModel: '=',
+      play: '&'
+    },
+    templateUrl: '/views/nprListItem.html',
+    link: function(scope, ele, attr) {
+      scope.duration = scope.ngModel.audio[0].duration.$text;
+    }
+  }
+});
