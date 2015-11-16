8064d1b1f7364938e4d4ed3e965743d6cf847c7a
Jory Schmidt <joryschmidt1988@gmail.com>
2015-11-16 23:05:07 +0000

    sparkline controller

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ app.controller('PlayerController', function($scope, $http){
         $scope.audio.src = url;
         $scope.audio.play();
         $scope.playing = true;
-    }
+    };
 });
 
 app.controller('RelatedController', ['$scope', function($scope){
@@ -44,17 +44,50 @@ app.directive('nprLink', function() {
     link: function(scope, ele, attr) {
       scope.duration = scope.ngModel.audio[0].duration.$text;
     }
-  }
+  };
 });
 
 app.directive('ngSparkline', function() {
-    return {
-        restrict: 'A',
-        require: '^ngCity',
-        scope: {
-            ngCity: '@'
-        },
-        template: '<div class="sparkline"><h4>Weather for {{ ngCity }}</h4></div>'
-        
+  var url = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=imperial&cnt=14&APPID=0d3ca11cd9f77be1eec1b284667dd845&callback=JSON_CALLBACK&q=";
+
+  return {
+      restrict: 'A',
+      require: '^ngCity',
+      scope: {
+        ngCity: '@'
+    },
+    template: '<div class="sparkline"><h4>Weather for {{ngCity}}</h4><div class="graph"></div></div>',
+    controller: ['$scope', '$http', function($scope, $http) {
+      $scope.getTemp = function(city) {
+        $http({
+          method: 'JSONP',
+          url: url + city
+        }).success(function(data) {
+          var weather = [];
+          angular.forEach(data.list, function(value){
+            weather.push(value);
+          });
+          $scope.weather = weather;
+        });
+      };
+    }],
+    link: function(scope, iElement, iAttrs, ctrl) {
+      scope.getTemp(iAttrs.ngCity);
+      scope.$watch('weather', function(newVal) {
+        // the `$watch` function will fire even if the
+        // weather property is undefined, so we'll
+        // check for it
+        if (newVal) {
+          var highs = [],
+              width   = 200,
+              height  = 80;
+
+          angular.forEach(scope.weather, function(value){
+            highs.push(value.temp.max);
+          });
+          // chart
+        }
+      });
     }
+  };
 });
