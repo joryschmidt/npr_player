47302da5d60e94c98024978c51c02d5130a65517
Jory Schmidt <joryschmidt1988@gmail.com>
2015-11-17 22:06:12 +0000

    obligatory commit

--- js/main.js
+++ js/main.js
@@ -55,39 +55,39 @@ app.directive('ngSparkline', function() {
       require: '^ngCity',
       scope: {
         ngCity: '@'
-    },
-    template: '<div class="sparkline"><h4>Weather for {{ngCity}}</h4><div class="graph"></div></div>',
-    controller: ['$scope', '$http', function($scope, $http) {
-      $scope.getTemp = function(city) {
-        $http({
-          method: 'JSONP',
-          url: url + city
-        }).success(function(data) {
-          var weather = [];
-          angular.forEach(data.list, function(value){
-            weather.push(value);
+      },
+      template: '<div class="sparkline"><h4>Weather for {{ngCity}}</h4><div class="graph"></div></div>',
+      controller: ['$scope', '$http', function($scope, $http) {
+        $scope.getTemp = function(city) {
+          $http({
+            method: 'JSONP',
+            url: url + city
+          }).success(function(data) {
+            var weather = [];
+            angular.forEach(data.list, function(value){
+              weather.push(value);
+            });
+            $scope.weather = weather;
           });
-          $scope.weather = weather;
+        };
+      }],
+      link: function(scope, iElement, iAttrs, ctrl) {
+        scope.getTemp(iAttrs.ngCity);
+        scope.$watch('weather', function(newVal) {
+          // the `$watch` function will fire even if the
+          // weather property is undefined, so we'll
+          // check for it
+          if (newVal) {
+            var highs = [],
+                width   = 200,
+                height  = 80;
+  
+            angular.forEach(scope.weather, function(value){
+              highs.push(value.temp.max);
+            });
+            // chart
+          }
         });
-      };
-    }],
-    link: function(scope, iElement, iAttrs, ctrl) {
-      scope.getTemp(iAttrs.ngCity);
-      scope.$watch('weather', function(newVal) {
-        // the `$watch` function will fire even if the
-        // weather property is undefined, so we'll
-        // check for it
-        if (newVal) {
-          var highs = [],
-              width   = 200,
-              height  = 80;
-
-          angular.forEach(scope.weather, function(value){
-            highs.push(value.temp.max);
-          });
-          // chart
-        }
-      });
-    }
+      }
   };
 });
