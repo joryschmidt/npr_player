4b2bc788985fe1eafdced248ce322c45142cf3fd
Jory Schmidt <joryschmidt1988@gmail.com>
2015-11-18 03:01:02 +0000

    fix and add stop button

--- js/main.js
+++ js/main.js
@@ -11,22 +11,22 @@ app.controller('PlayerController', ['$scope', 'nprService', 'player', function($
     });
 }]);
 
-  //app.controller('RelatedController', ['$scope', 'player',
-  // function($scope, player) {
-  // $scope.player = player;
-  //
-  // $scope.$watch('player.current', function(program) {
-  //   if (program) {
-  //     $scope.related = [];
-  //     angular.forEach(program.relatedLink, function(link) {
-  //       $scope.related.push({
-  //         link: link.link[0].$text, 
-  //         caption: link.caption.$text
-  //       });
-  //     });
-  //   }
-  // });
-  //}]);
+app.controller('RelatedController', ['$scope', 'player',
+ function($scope, player) {
+ $scope.player = player;
+
+ $scope.$watch('player.current', function(program) {
+   if (program) {
+     $scope.related = [];
+     angular.forEach(program.relatedLink, function(link) {
+       $scope.related.push({
+         link: link.link[0].$text, 
+         caption: link.caption.$text
+       });
+     });
+   }
+ });
+}]);
 
 app.directive('nprLink', function() {
   return {
